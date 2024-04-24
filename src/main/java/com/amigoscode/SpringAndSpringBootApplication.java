0b1e7fb276955c54d9d042f86b07c62e90ad3437
amigoscode <amigoscode@gmail.com>
2024-04-24 14:56:13 +0100

    starter branch

---
+++ src/main/java/com/amigoscode/SpringAndSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.amigoscode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAndSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAndSpringBootApplication.class, args);
+    }
+
+}
